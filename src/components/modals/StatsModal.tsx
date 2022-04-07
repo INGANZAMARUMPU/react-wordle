05ac6ea33cde1e2b6a9547b16062e1abba8e1831
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-04-08 01:46:38 +0200

    fair scoring

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -102,7 +102,7 @@ export const StatsModal = ({
         gameStats={gameStats}
         numberOfGuessesMade={numberOfGuessesMade}
       />
-      {isGameWon && gameScore && parseInt(gameScore) > 0 && (
+      {isGameWon && gameScore && (
         <>
           <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
             {INTERNATIONAL_COMPETITION_TEXT}
