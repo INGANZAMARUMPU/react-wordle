ad24c36ae27d4761ca9f70b16cf3c0a02a76db3b
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-04-10 16:36:55 +0200

    display field to ask for a meaning

--- src/components/modals/MeaningsModal.tsx
+++ src/components/modals/MeaningsModal.tsx
@@ -35,20 +35,19 @@ export const MeaningsModal = ({
           {proverb}
         </p>
       </div>
-      {false && (
-        <>
-          <div>
-            <p className="text-sm my-4 text-gray-500 dark:text-gray-300">
-              Woba uzi ico uwo mugani usigura ?
-            </p>
-          </div>
-          <div>
-            <div className="flex justify-center">
-              <textarea
-                value={meaning}
-                onChange={(event) => setMeaning(event.target.value)}
-                rows={5}
-                className="
+
+      <div>
+        <p className="text-sm my-4 text-gray-500 dark:text-gray-300">
+          Woba uzi ico uwo mugani usigura ?
+        </p>
+      </div>
+      <div>
+        <div className="flex justify-center">
+          <textarea
+            value={meaning}
+            onChange={(event) => setMeaning(event.target.value)}
+            rows={5}
+            className="
         form-control
         block
         w-full
@@ -64,33 +63,31 @@ export const MeaningsModal = ({
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-indigo-500 focus:outline-none"
-                placeholder="Insiguro y'umugani"
-              />
-            </div>
-          </div>
+            placeholder="Insiguro y'umugani"
+          />
+        </div>
+      </div>
 
-          <div className="mt-5 sm:mt-6 dark:text-white">
-            <button
-              type="button"
-              disabled={disabled}
-              className={classNames({
-                'mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white sm:text-sm':
-                  true,
-                'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500':
-                  !disabled,
-                'bg-slate-600': disabled,
-              })}
-              onClick={() => {
-                saveMeaningToDatabase(solution, proverb, meaning)
-                setIsMeaningsModalOpen(false)
-                showSuccessAlert('Urakoze ku ntererano yawe !')
-              }}
-            >
-              {SEND_TEXT}
-            </button>
-          </div>
-        </>
-      )}
+      <div className="mt-5 sm:mt-6 dark:text-white">
+        <button
+          type="button"
+          disabled={disabled}
+          className={classNames({
+            'mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white sm:text-sm':
+              true,
+            'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500':
+              !disabled,
+            'bg-slate-600': disabled,
+          })}
+          onClick={() => {
+            saveMeaningToDatabase(solution, proverb, meaning)
+            setIsMeaningsModalOpen(false)
+            showSuccessAlert('Urakoze ku ntererano yawe !')
+          }}
+        >
+          {SEND_TEXT}
+        </button>
+      </div>
     </BaseModal>
   )
 }
