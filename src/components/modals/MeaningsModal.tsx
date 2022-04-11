037c8f535cafd8ec9f946484a941f79d4b415a29
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-04-11 18:56:16 +0200

    add button to display textarea to ask for a meaning

--- src/components/modals/MeaningsModal.tsx
+++ src/components/modals/MeaningsModal.tsx
@@ -32,7 +32,7 @@ const Meanings = ({ meanings }: { meanings: string[] }) => (
   <>
     <div className="mt-8">
       <p className="text-m leading-6 font-medium text-gray-900 dark:text-gray-100">
-        Insiguro zimaze gutangwa
+        Insiguro zishoboka zimaze gutangwa
       </p>
     </div>
     <div className="flex flex-col divide-y">
@@ -52,6 +52,7 @@ export const MeaningsModal = ({
   showSuccessAlert,
 }: Props) => {
   const [meaning, setMeaning] = useState('')
+  const [displayMeaningTextArea, setDisplayMeaningTextArea] = useState(false)
   const disabled = meaning.length === 0
 
   return (
@@ -73,13 +74,28 @@ export const MeaningsModal = ({
           Woba uzi ico uwo mugani usigura ?
         </p>
       </div>
-      <div>
-        <div className="flex justify-center">
-          <textarea
-            value={meaning}
-            onChange={(event) => setMeaning(event.target.value)}
-            rows={5}
-            className="
+      <div className="my-5 sm:mt-6 dark:text-white">
+        <button
+          type="button"
+          className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          onClick={() => {
+            setDisplayMeaningTextArea(
+              displayMeaningTextArea === true ? false : true
+            )
+          }}
+        >
+          {displayMeaningTextArea ? 'OYA' : 'EGO'}
+        </button>
+      </div>
+      {displayMeaningTextArea && (
+        <>
+          <div>
+            <div className="flex justify-center">
+              <textarea
+                value={meaning}
+                onChange={(event) => setMeaning(event.target.value)}
+                rows={5}
+                className="
         form-control
         block
         w-full
@@ -95,31 +111,35 @@ export const MeaningsModal = ({
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-indigo-500 focus:outline-none"
-            placeholder="Insiguro y'umugani"
-          />
-        </div>
-      </div>
+                placeholder="Insiguro y'umugani"
+              />
+            </div>
+          </div>
+          <div className="mt-5 sm:mt-6 dark:text-white">
+            <button
+              type="button"
+              disabled={disabled}
+              className={classNames({
+                'mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white sm:text-sm':
+                  true,
+                'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500':
+                  !disabled,
+                'bg-slate-600': disabled,
+              })}
+              onClick={() => {
+                saveMeaningToDatabase(solution, proverb, meaning)
+                setIsMeaningsModalOpen(false)
+                setMeaning('')
+                setDisplayMeaningTextArea(false)
+                showSuccessAlert('Urakoze ku ntererano yawe !')
+              }}
+            >
+              {SEND_TEXT}
+            </button>
+          </div>
+        </>
+      )}
 
-      <div className="mt-5 sm:mt-6 dark:text-white">
-        <button
-          type="button"
-          disabled={disabled}
-          className={classNames({
-            'mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white sm:text-sm':
-              true,
-            'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500':
-              !disabled,
-            'bg-slate-600': disabled,
-          })}
-          onClick={() => {
-            saveMeaningToDatabase(solution, proverb, meaning)
-            setIsMeaningsModalOpen(false)
-            showSuccessAlert('Urakoze ku ntererano yawe !')
-          }}
-        >
-          {SEND_TEXT}
-        </button>
-      </div>
       {meanings.length > 0 && <Meanings meanings={meanings} />}
     </BaseModal>
   )
--- src/constants/meanings.ts
+++ src/constants/meanings.ts
@@ -5,7 +5,7 @@ export const MEANINGS = {
     'Imbere yuko ugira ico uvuze umuntu yobanza kwiyumvira hakuvuga vyinshi atakimazi kirimwo.\n Gushishoza imbere yukuvuga bigira ico vyungura.',
     'Ko ata kamaro kuvuga menshi kandi adakenewe.',
     'Si vyiza kuguma uvuga ibintu bitubaka upfuma uvuga gake gusa uvuga ivyubaka.',
-    'Menshi mabi\nUmuhungu: uri mwiza, ndashaka tuzokwicarane, niho vyanashoboka dusangir hanyuma, turabe iyo bija,tumenyane\nUmukobwa: oya ntibishoboka.\n\nRimwe ryiza\nUmuhungu: Maldives?\nUmukobwa: Ndagukunda gose... Twobonana ryari ?',
+    'Menshi mabi\nUmuhungu: Uri mwiza, ndashaka tuzokwicarane, niho vyanashoboka dusangire hanyuma, turabe iyo bija, tumenyane\nUmukobwa: Oya ntibishoboka.\n\nRimwe ryiza\nUmuhungu: Maldives ?\nUmukobwa: Ndagukunda gose... Twobonana ryari ?',
     'Ha kuvuga amajambo menshi adakenewe wotuma uvuga rimwe rifise akamaro.',
     'Aho kuvuga amajambo menshi atakamaro afitiye abo uriko urayabwira wokwinumira kuko bisumbwa no kuvuga ijambo rimwe rikagira akamaro ku bandi.',
     'Nivyiza kuvuga make.',
