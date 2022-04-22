a74c72d8a88472d7f35b0e1edb6669e861d916ea
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-04-22 13:23:14 +0200

    new meanings

--- src/constants/meanings.ts
+++ src/constants/meanings.ts
@@ -15,6 +15,8 @@ export const MEANINGS = {
     'Bashatse kuvuga yuko naho womara umwanya uvuga nabi muga ugaca uvuga neza bishobora guhindura ivyo wavuze vyose bikabonwa neza.',
   ],
   "Agasozi k'intahanurwa gasha abagabo babona": [
+    'Ifise insiguro imwe n uwundi uvuga uti: " Intabarirwa ibwirwa ar\'uko amaso atukuye."\nIyo wanse ko baguhanura ntiwizere ko bazokugarukira niwaja mu ngorane zatewe n\'impanuro wanse.',
+    'Iyo uhawe impanuro ariko ntuzikurikirize nyuma ugize ibibazo ntihagire uwuguserukako',
     'Iyo wanse guhanugwa hakiri kare bikakugiririra ingaruka mbi ni bake bakugarukirako kuko uba warahanuwe ukanka hakiri kare',
     'Intahanurwa ibwirwa nuko amaso atukuye',
     'Usa nuwundi mugani uvugango: "intabwirwa ibwirwa nuko amaso atukuye"',
@@ -27,5 +29,35 @@ export const MEANINGS = {
     'Iyo umuntu adahanurwa bakamuhanura ntiyumve arabona ingaruka zo kudahanurwa mu buzima bwiwe',
     'Iyo umuntu yanse kwumvira impanuro agira ibibazo abandi birorerera',
     "Ico ntahura nuko iyo wahanuwe kenshi ukanka iyo ushikiwe nivyago barakureka kuko waranse guhanurwa uyo mugani usa nuwundi uvuga kw'intabarirwa ibwirwa nuko amaso atukuye canke ngo ugutwi kw'impene kwumva kwokeje n'uyundi uvuga ngo wanka kugarura inka ikirihafi yarengana imirambi ukabira nkayo. Murakoze",
+  ],
+  'Imbwa ibura se ntibura shebuja': [
+    'Uwo mugani usigura ko imbwa ibwaguye ibi bwana vyayo bitamenya se ariko iyo umuntu ayituze birororshe kumenya shebuja nkaho vyomenya se',
+    'Bashatse kuvuga ko imbwa izi shebuja kurusha se, muyandi majambo umuntu avira ikintu adashobora kwibagirwa.',
+  ],
+  'Ico imbwa yanse ushira aho iraba': [
+    'Nukuvuga ko « niyo bishitse ugatanga ikintu nticakirwe ucugishira ahabona, kuko birashika bakagikenera munyuma nkuko kiba Kiri ahagaragara.»',
+  ],
+  'Uhisha mu nda imbwa ntimwiba': ['Uhisha ikintu uwundi ntakimenye'],
+  'Imbwa yarihuse ivyara ibihumye': [
+    'Gufata umwanya wo gushishoza muvyo dukora atakwihutagiza. Ugashira kumunzane ivyiza nibibi vyingingo ufashe imbere yo gushira mungiro',
+    'Birakenewe gufata umwanya mu gukora ibintu canke kubanza kwiyumvira mu gufata ingingo.',
+    'Vyerera igihe ntivyerera inzara',
+  ],
+  "Usangira ibanga n'imbwa ikarikwiza ibisaka": [
+    'Bisigura ko iyo ubwiye ibanga umuntu atagira ibanga aca arikwiragiza hose',
+    'Ntupfe uwo wizeye kuko bose si abagushakira ivyiza',
+  ],
+  'Imbwa ntigutwengera iba ishaka kukurya': [
+    'Bisigura ko mu kukugirira nabi, hari abatangura kukunezereza kugira baronke aho baca.',
+  ],
+  'Ukubitira imbwa gusutama arazimara': ['Kamere ka muntu ntawugahindura'],
+  'Abahigi benshi bayobeza imbwa': [
+    'Bisigura ko abantu benshi badafata ingingo, kenshi ko bataja inama ',
+  ],
+  'Uronse imbwa ayiharaye ayiharira iziko': [
+    'Iyuronse ikintu wari ugishashaye ugifata neza ',
+  ],
+  "Urya nk'inka ugapfa nk'imbwa": [
+    'Urya nabi ivyo warondeye ugapfa ataco ugifise ataco ugishobora kwimarira wari gushobora kubaho neza iyubika neza ugakoresha neza ',
   ],
 }
